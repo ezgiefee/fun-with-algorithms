24319692114395d7c9379fbbacdf1b4bc4f76d55
Ezgi Efe <ezgiefee@gmail.com>
2023-05-29 15:59:40 +0300

    Second approach

--- SquareValues.js
+++ SquareValues.js
@@ -5,6 +5,8 @@ same([1,2,3],[1,9])     //false
 same([1,2,1],[4,4,1])   //false
 */
 
+
+/*                                     First Approach                                                          */ 
 function same(arr1, arr2) {
   var result = false;
   if (arr1.length != arr2.length) return result;
@@ -20,4 +22,17 @@ function same(arr1, arr2) {
   return result;
 }
 
-console.log(same([1, 2, 3], [4, 4, 1]));
+//console.log(same([1, 2, 3], [4, 4, 1]));
+
+/*                                     Second Approach                                                          */ 
+function same1(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++){
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) return false;
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+console.log(same1([1, 2, 3], [4, 9, 1]));
