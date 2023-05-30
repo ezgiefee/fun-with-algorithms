bde9b42f8927a10285b470b73db4f02cbc55d8d1
Ezgi Efe <ezgiefee@gmail.com>
2023-05-30 23:39:10 +0300

    new approaches

--- MixedSolutions/FrequencyMultiplePointers.js
+++ MixedSolutions/FrequencyMultiplePointers.js
@@ -1,6 +1,6 @@
 /* Frequency Counter / Multiple Pointers - areThereDuplicates
 Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks 
-whether there are any duplicates among the arguments passed in.  You can solve this using the frequency 
+whether there are any duplicates among the arguments passed in. You can solve this using the frequency 
 counter pattern OR the multiple pointers pattern.
 
 areThereDuplicates(1, 2, 3) // false
@@ -27,3 +27,42 @@ function areThereDuplicates(...values) {
   return false;
 }
 console.log(areThereDuplicates('a', 'b', 'c', 'a'));
+
+/****************************              Second Approach             ****************************/ 
+function areThereDuplicates1() {
+  let collection = {}
+  for(let val in arguments){
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+  }
+  for(let key in collection){
+    if(collection[key] > 1) return true
+  }
+  return false;
+}
+
+console.log(areThereDuplicates1(1, 2, 3));
+
+/****************************              Third Approach             ****************************/ 
+function areThereDuplicates2(...args) {
+  // Two pointers
+  args.sort((a,b) => a > b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
+  }
+  return false
+}
+console.log(areThereDuplicates2('a', 'b', 'c', 'a'));
+
+/****************************              Fourth Approach             ****************************/ 
+
+function areThereDuplicates3() {
+  return new Set(arguments).size !== arguments.length;
+}
+
+console.log(areThereDuplicates2('a', 'b', 'c', 'a'));
