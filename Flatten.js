520fd288d1670c2cc2a2da07055f25953cda5de3
Ezgi Efe <ezgiefee@gmail.com>
2023-06-20 14:17:11 +0300

    recursive flatten algorithm

---
+++ Flatten.js
@@ -0,0 +1,21 @@
+/* Write a recursive function called flatten which accepts an array of arrays and returns 
+a new array with all values flattened. */
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+
+function flatten(oldArr){
+  var newArr = [];
+  	for(var i = 0; i < oldArr.length; i++){
+    	if(Array.isArray(oldArr[i])){
+        newArr = newArr.concat(flatten(oldArr[i]));
+    	} else {
+        newArr.push(oldArr[i]);
+    	}
+  } 
+  return newArr;
+}
+
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
